6f213afdca23d571557905394f741c7996b139db
Kevin Berridge <kevin.w.berridge@gmail.com>
2011-04-29 23:02:29 -0400

    tests command runner does nothing if prj file can't be found

--- spec/command_runner_spec.rb
+++ spec/command_runner_spec.rb
@@ -7,9 +7,9 @@ describe CommandRunner do
     @in_file_path = 'some/file/path/tofile.cs'
     @expected_rel_path = 'path\\tofile.cs'
 
+    @full_prj_path = '/a/full/path/to/some/file/prjfile.csproj'
+
     @finder = mock('finder')
-    @finder.stub!(:find_from).
-      and_return('/a/full/path/to/some/file/prjfile.csproj')
     PrjfileFinder.stub!(:new).and_return(@finder)
 
     @prjfile = mock('prjfile')
@@ -20,12 +20,30 @@ describe CommandRunner do
   end
 
   it "adds file on add" do 
+    @finder.stub!(:find_from).and_return(@full_prj_path)
+
     @prjfile.should_receive(:add).with(@expected_rel_path)
     subject.run ['add', @in_file_path]
   end
 
+  it "does nothing on add when prj file not found" do
+    @finder.stub!(:find_from).and_return(nil)
+
+    @prjfile.should_not_receive(:add)
+    subject.run ['add', @in_file_path]
+  end
+
   it "removes file on remove" do
+    @finder.stub!(:find_from).and_return(@full_prj_path)
+
     @prjfile.should_receive(:remove).with(@expected_rel_path)
+    subject.run ['remove', @in_file_path]
+  end
+
+  it "does nothing on remove when prj file not found" do
+    @finder.stub!(:find_from).and_return(nil)
+
+    @prjfile.should_not_receive(:add)
     subject.run ['remove', @in_file_path]
   end
 end
